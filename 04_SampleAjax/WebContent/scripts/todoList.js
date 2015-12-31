a782c0cc2a43b7e673e72965b1833ac3a3f79a8c
Veselin Dechev <veskopos@mail.bg>
2015-12-31 14:25:03 +0200

    Edit Task - HomeWork

--- 04_SampleAjax/WebContent/scripts/todoList.js
+++ 04_SampleAjax/WebContent/scripts/todoList.js
@@ -19,17 +19,20 @@ $(document).ready(function() {
 			dataType: "json"
 		});
 	}
+	
 	function readTask(taskId) {
 		return $.ajax(taskEndpoint(taskId), {
 			method: "GET",
 			dataType: "json"
 		});
 	}
+	
 	function showTaskView(task) {
 		$("#readPanel .task-title").text(task.title);
 		$("#readPanel .task-description").text(task.description);
 		showPanel("readPanel");
 	}
+	
 	function reloadTasks() {
 		listTasks().then(function(response) {
 			function addTaskToList(task) {
@@ -43,15 +46,49 @@ $(document).ready(function() {
 			_.forEach(response, addTaskToList);
 		});
 	}
+	
+	function Form(task) {
+ 		$("#updatePanel input").val(task.title);
+ 		$("#updatePanel textarea").val(task.description);
+ 	}
+ 	
+ 	function Edited(taskId) {
+ 		var task = {
+ 			title: $("#updatePanel input").val(),
+ 			description: $("#updatePanel textarea").val()
+ 		}
+ 		
+ 		$.ajax(taskEndpoint(taskId), {
+ 			method: "PUT",
+ 			contentType: "application/json; charset=utf-8",
+ 			data: JSON.stringify(task),
+ 			dataType: "json"
+ 		});
+ 	}
+
 	function attachHandlers() {
+		var taskId = null;
 		$(document).on("click", "#tasksList [data-task-id]", function() {
-			var taskId = $(this).attr("data-task-id");
+			taskId = $(this).attr("data-task-id");
 			readTask(taskId).then(showTaskView);
 		});
+		
 		$(".task-action-cancel").click(function() {
 			showPanel("emptyPanel");
 		});
+		
+		//----------Edit function-------------------
+		$("#readPanel .task-action-ok").click(function() {
+ 			showPanel("updatePanel");
+ 			readTask(taskId).then(Form);
+ 		});
+  		
+  		$("#updatePanel .task-action-ok").click(function() {
+  			Edited(taskId);
+  			window.location.reload();
+		});
 	}
+	
 	attachHandlers();
 	reloadTasks();
 });
