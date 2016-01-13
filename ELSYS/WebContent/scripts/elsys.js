f1615420470d3a0275e985c1fdae7b53b39b1b0e
Veselin Dechev <veskopos@mail.bg>
2016-01-14 01:35:58 +0200

    Elsys - HomeWork 5/20
    5) add new empty
    with id "dynamiccontent" as first child of div with id "footer"

--- ELSYS/WebContent/scripts/elsys.js
+++ ELSYS/WebContent/scripts/elsys.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 	console.log($("#footer a.tu").attr("title"));
 	console.log($("#col1 p").text());
 	$("#menu-top-level-menu").append("<li id=menu-item-1234><a href = #>new button");
+	$("#footer").append("<div id = dynamiccontent>");
 	
 	//alert("Under Construction!");
 });
